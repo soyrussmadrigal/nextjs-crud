0020acd88a1a9ba8a769a74e32f71b98b4119574
Russ Madrigal <71038002+rusbenmadrigal@users.noreply.github.com>
2023-04-06 09:06:54 -0600

    Navigation

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { useRouter } from "next/navigation";
+import { Link } from "next/link";
+
+export function Navbar() {
+  const router = useRouter();
+  return (
+    <header>
+      <Link href="/">
+        <h1>Tasks App</h1>
+      </Link>
+      <div>
+        <button onClick={() => router.push("/new")}>Add new task</button>
+      </div>
+    </header>
+  );
+}
